e1e36691a88a0e8d9bdced482c6dad9bfde51dc9
Kasfil Aziz <kasf@tuta.io>
2021-04-23 20:41:55 +0700

    disable italic and configs from nord

--- .config/nvim/lua/colorscheme/nord.lua
+++ .config/nvim/lua/colorscheme/nord.lua
@@ -1,21 +1,5 @@
 vim.cmd("colorscheme nordbuddy")
 
-vim.g.nord_bold = 1
-vim.g.nord_italic = 1
-vim.g.nord_italic_comments = 1
-
--- vim.api.nvim_exec([[
---     hi LspDiagnosticsVirtualTextError guifg=#bf616a
---     hi LspDiagnosticsVirtualTextWarning guifg=#ebcb8b
---     hi LspDiagnosticsVirtualTextInformation guifg=#88c0d0
---     hi LspDiagnosticsVirtualTextHint guifg=#88c0d0
---
---     hi LspDiagnosticsSignError guifg=#bf616a
---     hi LspDiagnosticsSignWarning guifg=#ebcb8b
---     hi LspDiagnosticsSignInformation guifg=#88c0d0
---     hi LspDiagnosticsSignHint guifg=#88c0d0
--- ]], false)
-
 -- barbar-nvim
 vim.api.nvim_exec([[
     hi! BufferCurrentMod guifg=#ebcb8b guibg=#2e3440
