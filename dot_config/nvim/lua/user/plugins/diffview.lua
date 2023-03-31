2bb69ac521fa990a6cf66bd63d137c940ac59edc
kasfil <kasf@tuta.io>
2023-03-31 23:01:38 +0700

    style: update indent type

--- dot_config/nvim/lua/user/plugins/diffview.lua
+++ dot_config/nvim/lua/user/plugins/diffview.lua
@@ -1,11 +1,13 @@
 return {
-  "sindrets/diffview.nvim",
-  lazy = false,
-  event = "BufReadPost",
-  opts = {
-    enhanced_diff_view = true,
-    hooks = {
-      view_opened = function(_) vim.opt_local.equalalways = true end,
-    },
-  }
+	"sindrets/diffview.nvim",
+	lazy = false,
+	event = "BufReadPost",
+	opts = {
+		enhanced_diff_view = true,
+		hooks = {
+			view_opened = function(_)
+				vim.opt_local.equalalways = true
+			end,
+		},
+	},
 }
