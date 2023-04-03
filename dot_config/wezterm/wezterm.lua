97dc4e26cc21411a5699d903435ec2f464db2b35
kasfil <kasf@tuta.io>
2023-04-04 05:13:45 +0700

    remove window padding

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -63,6 +63,8 @@ return {
 
 	font_size = 10.7,
 
+	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	tab_bar_at_bottom = true,
