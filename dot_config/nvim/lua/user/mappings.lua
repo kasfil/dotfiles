58a863a157361692b6447ae4e521668cf3e9a2fb
kasfil <kasf@tuta.io>
2024-02-28 10:15:28 +0700

    update mappings

--- dot_config/nvim/lua/user/mappings.lua
+++ dot_config/nvim/lua/user/mappings.lua
@@ -1,15 +1,29 @@
 local utils = require "astronvim.utils"
+local mapping = utils.empty_map_table()
 
-local mapping = require("astronvim.utils").empty_map_table()
-
+-- Mapping prefix name
+mapping.n["<leader>r"] = { name = "Overseer" }
+mapping.n["<leader>x"] = { name = "Misc" }
 mapping.n["<leader>lp"] = { name = "Peek" }
-mapping.n["<leader>lN"] = { function() vim.lsp.inlay_hint(0) end, desc = "Toggle lsp inlay-hints" }
 
-mapping.t["<esc><esc>"] = { "<C-\\><C-n>:q<cr>", desc = "Terminal quit" }
+mapping.n["<leader>lN"] =
+  { function() vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled(0)) end, desc = "Toggle lsp inlay-hints" }
 
 if vim.fn.executable "btop" == 1 then
   mapping.n["<leader>tt"] = { function() utils.toggle_term_cmd "btop" end, desc = "ToggleTerm btop" }
 end
+
+if vim.fn.executable "lazygit" == 1 then
+  mapping.n["<leader>tg"] = { function() utils.toggle_term_cmd "lazygit" end, desc = "ToggleTerm lazygit" }
+end
+
+if vim.fn.executable "lazysql" == 1 then
+  mapping.n["<leader>ts"] = { function() utils.toggle_term_cmd "lazysql" end, desc = "ToggleTerm lazysql" }
+end
+
+-- Insert uuid
+mapping.n["<leader>xu"] = { 'a<C-r>=trim(system("uuidgen"))<CR><ESC>', desc = "insert uuid" }
+mapping.i["<C-x><C-u>"] = { '<C-r>=trim(system("uuidgen"))<CR>', desc = "insert uuid" }
 
 if vim.g.neovide then
   mapping.n["<C-v>"] = { '"+P', desc = "Paste from clipboard" }
