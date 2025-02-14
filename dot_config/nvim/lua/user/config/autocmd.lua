2a1794614bdc72e351fd63d986e9dfa125834641
kasfil <kasf@tuta.io>
2025-02-14 22:49:14 +0700

    fix: remove gitcommit from filetype close autocmd

--- dot_config/nvim/lua/user/config/autocmd.lua
+++ dot_config/nvim/lua/user/config/autocmd.lua
@@ -103,14 +103,13 @@ vim.api.nvim_create_autocmd("FileType", {
     "tsplayground",
     "dap-float",
     "dap-repl",
-    "gitcommit",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.schedule(function()
       vim.keymap.set("n", "q", function()
         vim.cmd "close"
-        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+        pcall(_G.MiniBufremove, event.buf)
       end, {
         buffer = event.buf,
         silent = true,
