76139349c200f04bbf4917ad94c2a0c03922ef6d
kasfil <kasf@tuta.io>
2025-02-14 22:53:34 +0700

    chore: remove redundant help filetype config

--- dot_config/nvim/after/ftplugin/help.lua
+++
@@ -1,3 +0,0 @@
-pcall(vim.keymap.del, "n", "]b", { buffer = true })
-pcall(vim.keymap.del, "n", "[b", { buffer = true })
-vim.keymap.set("n", "q", "<Cmd>q<CR>", { desc = "quit", buffer = 0, nowait = true })
