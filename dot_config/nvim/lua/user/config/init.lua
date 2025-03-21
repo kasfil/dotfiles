408d7523bdf32503bb614d5e309d6ba830eb6dd7
kasfil <kasf@tuta.io>
2025-03-21 10:50:06 +0700

    feat: optional neovide config

--- dot_config/nvim/lua/user/config/init.lua
+++ dot_config/nvim/lua/user/config/init.lua
@@ -3,3 +3,5 @@ require "user.config.option"
 require "user.config.setup"
 require "user.config.mapping"
 require "user.config.autocmd"
+-- include neovide additional config
+if vim.g.neovide then require "user.config.neovide" end
---
+++ dot_config/nvim/lua/user/config/neovide.lua
@@ -0,0 +1,6 @@
+vim.g.neovide_cursor_vfx_mode = ""
+vim.g.neovide_floating_corner_radius = 0.0
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_floating_shadow = false
+
+vim.opt.linespace = 5
