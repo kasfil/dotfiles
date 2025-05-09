ba591d2a7595d88c541788b4e387e4c4726217d6
kasfil <kasf@tuta.io>
2025-05-09 11:03:04 +0700

    feat: disable bold text

--- dot_config/nvim/lua/user/plugins/colorscheme.lua
+++ dot_config/nvim/lua/user/plugins/colorscheme.lua
@@ -5,7 +5,6 @@ return {
     priority = 1000,
     init = function()
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_enable_bold = 1
       vim.g.gruvbox_material_float_style = "dim"
       vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
       vim.g.gruvbox_material_inlay_hints_background = "dimmed"
