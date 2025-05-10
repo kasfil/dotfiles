aef786fd33e70fd6177ddfac4e8424b9bb5d1e47
kasfil <kasf@tuta.io>
2025-05-10 16:25:56 +0700

    feat: custom rule for autopairs

--- dot_config/nvim/lua/user/plugins/autopairs.lua
+++ dot_config/nvim/lua/user/plugins/autopairs.lua
@@ -2,15 +2,24 @@
 -- https://github.com/windwp/nvim-autopairs
 
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
   -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
+  dependencies = { "hrsh7th/nvim-cmp" },
   config = function()
-    require('nvim-autopairs').setup {}
+    local npairs = require "nvim-autopairs"
+    local Rule = require "nvim-autopairs.rule"
+    local cond = require "nvim-autopairs.conds"
+
+    npairs.setup {}
     -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-    local cmp = require 'cmp'
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    local cmp = require "cmp"
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    npairs.add_rules {
+      Rule("<", ">", { "rust", "rs" }),
+      Rule("|", "|", { "rust", "rs" }):with_pair(cond.before_text "("):with_move(cond.done()),
+    }
   end,
 }
